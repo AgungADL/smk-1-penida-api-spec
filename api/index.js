51e125b0d01d5b0476592411cd4120ae34f2653a
Agung <agungcoygamez123@gmail.com>
2025-09-14 23:32:45 +0700

    fixing routing

--- api/index.js
+++ api/index.js
@@ -3,23 +3,30 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const path = require("path");
 const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// Perbaikan: Konfigurasi Swagger
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
-app.get("/swagger.yaml", (req, res) => {
-    res.sendFile(path.join(__dirname, "swagger.yaml"));
-});
-
-const swaggerOption = {
-    swaggerUrl: '/swagger.yaml'
+const swaggerOptions = {
+  // Ini harus menjadi URL di mana Express Anda akan menyajikan file YAML
+  swaggerUrl: '/swagger.yaml',
 };
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOption));
+// Rute untuk menyajikan file YAML itu sendiri
+app.get('/swagger.yaml', (req, res) => {
+  res.sendFile(path.join(__dirname, 'swagger.yaml'));
+});
 
+// Ini adalah rute untuk dokumentasi Swagger UI
+// Sekarang Swagger UI akan tahu harus mengambil data dari '/swagger.yaml'
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
+
+// Rute 'hello' yang sudah benar
 app.get('/hello', (req, res) => {
   res.status(200).json({ message: 'Hello from Vercel!' });
 });
